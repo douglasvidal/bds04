71e2ec1fb4cdc725da801fd26e37ed0b442ce178
Douglas Vidal <dougfvidal@gmail.com>
2021-10-16 18:22:15 -0300

    Challenge has been finished

---
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -0,0 +1,40 @@
+package com.devsuperior.bds04.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.EventRepository;
+
+@Service
+public class EventService {
+
+	@Autowired
+	private EventRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<EventDTO> findAll(PageRequest pageRequest) {
+		Page<Event> pages = repository.findAll(pageRequest);
+		return pages.map(entity -> new EventDTO(entity));
+	}
+	
+	@Transactional
+	public EventDTO insert(EventDTO dto) {
+		Event entity = new Event();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new EventDTO(entity);
+	}
+	
+	private void copyDtoToEntity(EventDTO dto, Event entity) {
+		entity.setName(dto.getName());
+		entity.setDate(dto.getDate());
+		entity.setUrl(dto.getUrl());
+		entity.setCity(new City(dto.getCityId(), null));		
+	}
+}
